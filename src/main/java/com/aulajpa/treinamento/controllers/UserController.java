69cbdc7b1fe846dd3877a7300b76a1a8cd2bde18
elisson <elisson@elisson-PC>
2021-04-21 15:59:01 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/com/aulajpa/treinamento/controllers/UserController.java

--- src/main/java/com/aulajpa/treinamento/controllers/UserController.java
+++ src/main/java/com/aulajpa/treinamento/controllers/UserController.java
@@ -9,9 +9,13 @@ import com.aulajpa.treinamento.entities.User;
 import com.aulajpa.treinamento.repository.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,14 +25,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-    
+
     @Autowired
     private UserRepository repository;
-    
+
+    public int getPageSize() {
+        return 25;
+    }
+
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> result = repository.findAll();
         return ResponseEntity.ok(result);
     }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+        Page<User> result = repository.findAll(pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/seach-salary")
+    public ResponseEntity<Page<User>> searchBySalary(
+            @RequestParam(defaultValue = "0") Double minSalary,
+            @RequestParam(defaultValue = "100000000") Double maxSalary,
+            @PageableDefault(size = 40) Pageable pageable) {
+        Page<User> result = repository.searchSalary(minSalary, maxSalary, pageable);
+        return ResponseEntity.ok(result);
+    }
     
+    @GetMapping(value = "/seach-name")
+    public ResponseEntity<Page<User>> searchByName(
+            @RequestParam(defaultValue = "") String name,
+            @PageableDefault(size = 40) Pageable pageable) {
+        Page<User> result = repository.findByNameContainingIgnoreCase(name, pageable);
+        return ResponseEntity.ok(result);
+    }
+
 }
--- src/main/java/com/aulajpa/treinamento/repository/UserRepository.java
+++ src/main/java/com/aulajpa/treinamento/repository/UserRepository.java
@@ -6,12 +6,23 @@
 package com.aulajpa.treinamento.repository;
 
 import com.aulajpa.treinamento.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author elisson
  */
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    @Query("SELECT obj FROM  User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+    public Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+
+    @Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%') ) ")
+    public Page<User> searchName(String name, Pageable pageable);
+    
+    public Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
     
 }
